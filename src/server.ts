cd5dad6e029234bc00c7ab96e94871203d7bc442
BadMisterH <60579407+BadMisterH@users.noreply.github.com>
2026-02-16 21:00:44 +0100

    test

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,9 @@ import app from "./app";
 import { startAutoRelanceService } from "./services/autoRelanceService";
 import { gmailMultiUserService } from "./services/gmailMultiUserService";
 
-const PORT = Number(process.env.PORT);
+const PORT = Number(process.env.PORT) || 3000;
 
-console.log("ENV PORT =", process.env.PORT);
-
-if (!PORT) {
-  throw new Error("PORT not defined");
-}
+console.log("ENV PORT =", process.env.PORT ?? "(not set, using 3000)");
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on ${PORT}`);
